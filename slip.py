d889a1fe86d19c1e1dfa15b2c2ad6e2b307fb0fd
matheusstrito <matheus.m.salomao@hotmail.com>
2025-07-03 11:02:00 -0300

    P4 finalizada

--- slip.py
+++ slip.py
@@ -1,64 +1,66 @@
 class CamadaEnlace:
     ignore_checksum = False
 
-    def __init__(self, linhas_seriais):
-        """
-        Inicia uma camada de enlace com um ou mais enlaces, cada um conectado
-        a uma linha serial distinta. O argumento linhas_seriais é um dicionário
-        no formato {ip_outra_ponta: linha_serial}. O ip_outra_ponta é o IP do
-        host ou roteador que se encontra na outra ponta do enlace, escrito como
-        uma string no formato 'x.y.z.w'. A linha_serial é um objeto da classe
-        PTY (vide camadafisica.py) ou de outra classe que implemente os métodos
-        registrar_recebedor e enviar.
-        """
-        self.enlaces = {}
-        self.callback = None
-        # Constrói um Enlace para cada linha serial
-        for ip_outra_ponta, linha_serial in linhas_seriais.items():
-            enlace = Enlace(linha_serial)
-            self.enlaces[ip_outra_ponta] = enlace
-            enlace.registrar_recebedor(self._callback)
+    def __init__(self, serial_interfaces):
+        self.upper_layer_handler = None
+        self.active_links = {
+            dest_ip: Enlace(serial_if)
+            for dest_ip, serial_if in serial_interfaces.items()
+        }
+        for link in self.active_links.values():
+            link.set_receiver(self._handle_reception)
 
-    def registrar_recebedor(self, callback):
-        """
-        Registra uma função para ser chamada quando dados vierem da camada de enlace
-        """
-        self.callback = callback
+    def registrar_recebedor(self, handler):
+        self.upper_layer_handler = handler
 
-    def enviar(self, datagrama, next_hop):
-        """
-        Envia datagrama para next_hop, onde next_hop é um endereço IPv4
-        fornecido como string (no formato x.y.z.w). A camada de enlace se
-        responsabilizará por encontrar em qual enlace se encontra o next_hop.
-        """
-        # Encontra o Enlace capaz de alcançar next_hop e envia por ele
-        self.enlaces[next_hop].enviar(datagrama)
+    def enviar(self, packet, destination_address):
+        if destination_address in self.active_links:
+            self.active_links[destination_address].send(packet)
 
-    def _callback(self, datagrama):
-        if self.callback:
-            self.callback(datagrama)
+    def _handle_reception(self, packet):
+        if self.upper_layer_handler:
+            self.upper_layer_handler(packet)
 
 
 class Enlace:
-    def __init__(self, linha_serial):
-        self.linha_serial = linha_serial
-        self.linha_serial.registrar_recebedor(self.__raw_recv)
+    def __init__(self, serial_port):
+        self.serial_port = serial_port
+        self.serial_port.registrar_recebedor(self._raw_data_handler)
+        self.reception_buffer = b''
+        self.packet_receiver = None
 
-    def registrar_recebedor(self, callback):
-        self.callback = callback
+    def set_receiver(self, callback):
+        self.packet_receiver = callback
 
-    def enviar(self, datagrama):
-        # TODO: Preencha aqui com o código para enviar o datagrama pela linha
-        # serial, fazendo corretamente a delimitação de quadros e o escape de
-        # sequências especiais, de acordo com o protocolo CamadaEnlace (RFC 1055).
-        pass
+    def _escape_payload(self, payload):
+        payload = payload.replace(b'\xdb', b'\xdb\xdd')
+        payload = payload.replace(b'\xc0', b'\xdb\xdc')
+        return payload
 
-    def __raw_recv(self, dados):
-        # TODO: Preencha aqui com o código para receber dados da linha serial.
-        # Trate corretamente as sequências de escape. Quando ler um quadro
-        # completo, repasse o datagrama contido nesse quadro para a camada
-        # superior chamando self.callback. Cuidado pois o argumento dados pode
-        # vir quebrado de várias formas diferentes - por exemplo, podem vir
-        # apenas pedaços de um quadro, ou um pedaço de quadro seguido de um
-        # pedaço de outro, ou vários quadros de uma vez só.
-        pass
+    def send(self, data_packet):
+        escaped_packet = self._escape_payload(data_packet)
+        frame = b'\xc0' + escaped_packet + b'\xc0'
+        self.serial_port.enviar(frame)
+
+    def _unescape_payload(self, escaped_payload):
+        payload = escaped_payload.replace(b'\xdb\xdc', b'\xc0')
+        payload = payload.replace(b'\xdb\xdd', b'\xdb')
+        return payload
+
+    def _raw_data_handler(self, incoming_data):
+        self.reception_buffer += incoming_data
+        
+        frames = self.reception_buffer.split(b'\xc0')
+        self.reception_buffer = frames[-1]
+
+        for frame_content in frames[:-1]:
+            if not frame_content:
+                continue
+            
+            try:
+                if self.packet_receiver:
+                    datagram = self._unescape_payload(frame_content)
+                    self.packet_receiver(datagram)
+            except:
+                import traceback
+                traceback.print_exc()
